75a896685bc20d50f4dec351a6be9268d6f148c7
Blackadder98 <Sboterog99@gmail.com>
2024-09-04 17:44:27 -0500

    commit inicial

---
+++ src/main/test/com/miempresa/miapp/ServicioVeterinarioTest.java
@@ -0,0 +1,14 @@
+package main.test.com.miempresa.miapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ServicioVeterinarioTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
