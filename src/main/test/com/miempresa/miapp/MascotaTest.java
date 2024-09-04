6acd124c15bc57563bc5b0df878775e219527eca
Blackadder98 <Sboterog99@gmail.com>
2024-09-04 18:01:11 -0500

    Test getNombre de class mascota

--- src/main/test/com/miempresa/miapp/MascotaTest.java
+++ src/main/test/com/miempresa/miapp/MascotaTest.java
@@ -2,13 +2,24 @@ package main.test.com.miempresa.miapp;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import main.java.com.miempresa.miapp.Mascota;
 
 class MascotaTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	  Mascota mascota;
 
+	    @BeforeEach
+	    void antes() {
+	        mascota = new Mascota("toby", "perro", 10);
+	    }
+
+	    @Test
+	    void testGetUsername() {
+	        String nombre = mascota.getNombre();
+	        assertEquals("toby", nombre);
+	    }
+	
 }
