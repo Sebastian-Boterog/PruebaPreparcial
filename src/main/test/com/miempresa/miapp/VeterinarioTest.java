2c62d9e00d611ceb6f9a042148b82d0989df20bb
Blackadder98 <Sboterog99@gmail.com>
2024-09-04 18:24:48 -0500

    Test agregar mascota

--- src/main/test/com/miempresa/miapp/VeterinarioTest.java
+++ src/main/test/com/miempresa/miapp/VeterinarioTest.java
@@ -2,13 +2,37 @@ package main.test.com.miempresa.miapp;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import main.java.com.miempresa.miapp.Mascota;
+import main.java.com.miempresa.miapp.Veterinario;
 
 class VeterinarioTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+
+    private Veterinario veterinario;
+
+    @BeforeEach
+    void Antes() {
+        veterinario = new Veterinario();
+    }
+
+    @Test
+    void testAgregarMascota() {
+        Mascota mascota = new Mascota("Rex", "perro", 4);
+        veterinario.agregarMascota(mascota);
+
+        assertEquals(1, veterinario.obtenerMascotas().size(), "El número de mascotas debería ser 1");
+
+        Mascota mascotaAgregada = veterinario.obtenerMascotas().get(0);
+        assertEquals("Rex", mascotaAgregada.getNombre(), "El nombre de la mascota debería ser Rex");
+        assertEquals("perro", mascotaAgregada.getTipo(), "El tipo de la mascota debería ser perro");
+        assertEquals(4, mascotaAgregada.getEdad(), "La edad de la mascota debería ser 4");
+    }
+    
+    
 
 }
